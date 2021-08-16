1429adb6919dabff4d1820aaf8484e71b3f85e46
Patrick Brennan <pbrennanmusic@gmail.com>
2021-08-16 16:55:59 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :recipes, only: [:new, :create, :show, :edit, :update]
+  resources :ingredients, only: [:new, :create, :show, :edit, :update]
 end
